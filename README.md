115c4dbbd1a858af1448ed4bd607e42f92fdeb64
Zack Stout <zackstout@gmail.com>
2018-08-24 00:31:31 -0500

    considering a new tack that may be better

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 I stumbled upon this [dataset](https://www.kaggle.com/datasnaek/chess) of previously-played chess games. My first idea is to build a tree of possible openings and variations.
 
 I didn't include the CSV in this repository; you'll need to download the data on your own and add it to this project (it should be called `games.csv`).
+
+## Notes to self
+- I've noticed that I'm still writing Python the same way -- the JS way (i.e. the wrong way). Need to study those Pythonic patterns until their use-cases are ingrained.
+- This is a challenge. Maybe I'm overthinking it -- could we just pass in a string of previous moves and then query the database for all those lines that begin like that? We could! This raises the question -- why else did we want the tree structure in the first place?
--- index.py
+++ index.py
@@ -15,7 +15,7 @@ def addToDF():
     df['opening_moves_text'] = [" ".join(df['moves_list'][i][ : df['opening_ply'][i]]) for i in range(len(df))]
 
 addToDF()
-# print(df.head(15))
+print(df.head(15))
 
 
 # This won't quite work, because some names (e.g. "Queen's Pawn" refer to both 1- and 2-move openings):
@@ -57,33 +57,34 @@ def generateOneMoveOpenings():
 
 
 print('hi there')
-generateOneMoveOpenings()
-print(getMovesFromName("Queen's Pawn", 1))
-
+# generateOneMoveOpenings()
+print(getMovesFromName("Queen's Pawn", 2))
 
 
 
 def generateTwoMoveOpenings():
     d2_open = df[df['opening_ply'] == 2]
 
+    # We are already being really inefficient, looping through this whole list. Could we first turn it into a set?
     for d in d2_open['opening_name']:
         o_moves = getMovesFromName(d, 2) # e.g. "e4 d4"
 
         for o, v in openings_tree.items(): # iteritems was removed in python3!
             o_move = v['move'] # e.g. "e4"
 
-            if o_moves.startswith(o_move) and not hasattr(o, d):
+            # If current move started with other move, add this to that other move's dictionary:
+            if o_moves.startswith(o_move) and not hasattr(o, d): # We don't need the .str startswith method, because it's already a string!
                 v[d] = dict()
                 v[d]['move'] = o_moves
-
-            # curr_move = getMovesFromName()
-            # move = df[df['opening_name'] == o]
-
+            # We'll need an else here in order to capture those 2-move openings that don't have a 1-move named start (if such there be).
+            # Basically, we need to be thoughtful about how to store this data in the most efficient way.
 
     print("TREE 2: ", openings_tree)
 
 
-generateTwoMoveOpenings()
+# generateTwoMoveOpenings()
+
+
 
 
 # print(d_open.groupby('opening_name').count())
@@ -99,8 +100,18 @@ all_openings = df.groupby('opening_name')
 
 
 
+def findLinesStartingWith(open):
+    # return 'hi'
+    result = dict()
+    all_moves = df[df['moves'].str.startswith('d4')]
+    result['group'] = all_moves.groupby('opening_name').count() # I'd like to know why this changes every column's value...
+    # result['count'] = len(all_moves) # It already has this
 
+    # print(result)
+    return result
 
+res = findLinesStartingWith('d4')
+print(res)
 
 
 
